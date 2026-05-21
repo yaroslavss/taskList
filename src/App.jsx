238ac0d70681fdb4d0562b87528890d7f4a1fca2
yaroslavss <yaroslav.syuzev@gmail.com>
2026-05-21 14:32:08 +0400

    Add functionality to taskList(), taskItem()

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,9 @@ function App() {
     setTasks([...tasks, { ...task, completed: false, id: Date.now() }]);
   }
 
+  const activeTasks = tasks.filter((task) => !task.completed);
+  const completedTasks = tasks.filter((task) => task.completed);
+
   return (
     <div className="app">
       <div className="task-container">
@@ -44,7 +47,7 @@ function App() {
           <button className="sort-button">By Date</button>
           <button className="sort-button">By Priority</button>
         </div>
-        {openSection.tasks && <TaskList />}
+        {openSection.tasks && <TaskList activeTasks={activeTasks} />}
       </div>
 
       <div className="completed-task-container">
@@ -103,30 +106,30 @@ function TaskForm({ addTask }) {
   );
 }
 
-function TaskList() {
+function TaskList({ activeTasks }) {
   return (
     <ul className="task-list">
-      <TaskItem />
+      {activeTasks.map((task) => (
+        <TaskItem task={task} key={task.id} />
+      ))}
     </ul>
   );
 }
 
 function CompletedTaskList() {
-  return (
-    <ul className="completed-task-list">
-      <TaskItem />
-    </ul>
-  );
+  return <ul className="completed-task-list">{/* <TaskItem /> */}</ul>;
 }
 
-function TaskItem() {
+function TaskItem({ task }) {
+  const { title, priority, deadline } = task || {};
+
   return (
-    <li className="task-item">
+    <li className={`task-item ${priority.toLowerCase()}`}>
       <div className="task-info">
         <div>
-          Title <strong>Medium</strong>
+          {title} <strong>{priority}</strong>
         </div>
-        <div className="task-deadline">Due: {new Date().toLocaleString()}</div>
+        <div className="task-deadline">Due: {deadline}</div>
       </div>
       <div className="task-buttons">
         <button className="complete-button">Complete</button>
